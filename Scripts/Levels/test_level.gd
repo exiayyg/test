d273f9aa664c6dc67917579eafff621e9eefe8a4
Red0tsnow <zhuanghwchin@outlook.com>
2026-01-17 13:25:09 +0800

    设置随机生成敌人

--- Scripts/Levels/test_level.gd
+++ Scripts/Levels/test_level.gd
@@ -6,6 +6,11 @@ extends Node2D
 #预加载普通敌人场景
 @onready var player: CharacterBody2D = $Player
 
+#设置生成地图的边界
+@export var map_width: float = 1000.0 
+@export var map_height: float =  600.0
+@export var spawn_margin: float = 50.0
+
 
 func _physics_process(_delta: float) -> void:
 	Global.player_position = player.global_position#实时更新玩家全局位置
@@ -18,7 +23,34 @@ func _on_player_shoot_string(player_position: Vector2, mouse_position: Vector2) 
 	bullet.set_direction(bullet_direction)#设置子弹发射方向
 	$Bullets.add_child(bullet)#将实例化的子弹场景存入Bullers节点中
 
-func create_enemies(spwan_position: Vector2):#创建敌人方法，需要传入敌人生成位置（spwan_position）
+#生成敌人，没有给确切的生成位置就随机出生点生成
+func create_enemies(spawn_position: Vector2 = Vector2(0, 0)):#创建敌人方法，需要传入敌人生成位置（spawn_position）
 	var normal_enemy = normal_enemy_scene.instantiate()#在关卡中创建普通敌人实例
-	normal_enemy.global_position = spwan_position#获取敌人生成位置
+	if spawn_position == Vector2.ZERO:
+		spawn_position = GetRandomSpawnPositionOutsideMap()
+	normal_enemy.global_position = spawn_position#获取敌人生成位置
 	$Enemies.add_child(normal_enemy)#将实例化的敌人场景存入Enemies节点中
+
+#随机获取一个地图外的出生点
+func GetRandomSpawnPositionOutsideMap() -> Vector2:
+	var side = randi() % 4#0，1，2，3
+	var spawn_pos: Vector2 = Vector2.ZERO
+	
+	match side:
+		0:#上边
+			spawn_pos.x = randf_range(-map_width/2, map_width/2)
+			spawn_pos.y = -map_height/2 - spawn_margin
+		1:#右边
+			spawn_pos.x = map_width/2 + spawn_margin
+			spawn_pos.y = randf_range(-map_height/2, map_height/2)
+		2:#下边
+			spawn_pos.x = randf_range(-map_width/2, map_width/2)
+			spawn_pos.y = map_height/2 + spawn_margin
+		3:#左边
+			spawn_pos.x = -map_width/2 - spawn_margin
+			spawn_pos.y = randf_range(-map_height/2, map_height/2)
+	return spawn_pos
+
+
+func _on_spawn_timer_timeout() -> void:
+	create_enemies()
